abe35c58f8384559ead379818d58775c784a103b
Dominik Kaegi <kaegidominik@gmail.com>
2020-03-11 17:23:48 -0300

    clean-for-day-3

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,147 +1,109 @@
 import React, { useState } from "react";
 
-const initialTodos = [
-  {
-    task: "Workout at 7 am",
-    isDone: false,
-    id: "1"
-  },
-  {
-    task: "Prepare React Course",
-    isDone: false,
-    id: "2"
-  },
-  {
-    task: "Eat chocolate",
-    isDone: false,
-    id: "3"
-  }
-];
+function Input(props) {
+  const { onNewInput } = props;
 
-const Todo = () => {
-  const [todos, setTodos] = useState(initialTodos);
-
-  const newTodoHandler = newTodo => {
-    setTodos([...todos, newTodo]);
-  };
-
-  const deleteHandler = id => {
-    setTodos(todos => todos.filter(item => item.id !== id));
-  };
-
-  const toggleDoneHandler = id => {
-    setTodos(todos =>
-      todos.map(item => {
-        if (item.id === id) {
-          item.isDone = !item.isDone;
-        }
-        return item;
-      })
-    );
-  };
-  return (
-    <>
-      <div className="flex justify-center align-center font-sans">
-        <div className="w-2/6 md:w-3/6">
-          <div className="bg-gray-100 p-5 rounded shadow-lg mt-5">
-            <div>
-              <TodoInput onNewTodo={newTodoHandler} />
-            </div>
-            <div className={"mt-5"}>
-              <TodoList
-                todos={todos}
-                onDelete={deleteHandler}
-                onToggleDone={toggleDoneHandler}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <pre>
-        {JSON.stringify(
-          {
-            input,
-            todos
-          },
-          null,
-          2
-        )}
-      </pre> */}
-    </>
-  );
-};
-
-const TodoInput = ({ onNewTodo }) => {
   const [input, setInput] = useState("");
+  const inputChangeHandler = event => {
+    setInput(event.target.value);
+  };
 
-  const submitHandler = e => {
-    e.preventDefault();
-    if (input.length > 0) {
-      let newTodo = {
-        task: input,
-        isDone: false,
-        id: Math.floor(Math.random() * 1000000)
-      };
-
-      onNewTodo(newTodo);
-      setInput("");
+  const submitHandler = event => {
+    event.preventDefault();
+    if (input.length === 0) {
+      return;
     }
+    onNewInput(input);
+    setInput("");
   };
 
   return (
-    <div>
-      <form onSubmit={submitHandler} className="flex">
-        <input
-          name="todo"
-          className="bg-gray-200 rounded rounded-r-none p-3 flex-grow text-gray-700"
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          placeholder="Get things done...."
-        />
-        <button
-          type="submit"
-          className="p-3 bg-gray-300 rounded rounded-l-none text-green-600"
-        >
-          Add
-        </button>
-      </form>
-    </div>
+    <form
+      onSubmit={submitHandler}
+      style={{ padding: "10px", border: "1px solid #a0aec0", display: "flex" }}
+    >
+      <input
+        type="text"
+        placeholder="Add new todo"
+        value={input}
+        onChange={inputChangeHandler}
+        style={{ padding: "10px", flexGrow: 1 }}
+      />
+      <button
+        style={{
+          backgroundColor: "#edf2f7",
+          color: "#68d391",
+          padding: "10px"
+        }}
+        type="submit"
+      >
+        Add
+      </button>
+    </form>
   );
-};
+}
 
-const TodoList = ({ todos, onToggleDone, onDelete }) => {
+function TodoList(props) {
+  const { todos, onDelete } = props;
+
   if (todos.length === 0) {
-    return <div className="">No Todos</div>;
+    return <div style={{ fontSize: "1.25rem", padding: "10px" }}>No todos</div>;
   }
 
   return (
-    <div className="mt-5">
+    <div style={{ margin: "20px" }}>
       {todos.map(todo => {
-        return (
-          <div
-            name="todo"
-            className="bg-gray-200 text-gray-700 border-b-2 flex rounded"
-            key={todo.id}
-          >
-            <button
-              onClick={() => onDelete(todo.id)}
-              className="p-3 text-red-400"
-            >
-              DEL
-            </button>
-            <span
-              onClick={() => onToggleDone(todo.id)}
-              className={`inline-block cursor-pointer select-none p-3 pl-0 ${
-                todo.isDone ? "line-through text-gray-500" : ""
-              }`}
-            >
-              {todo.task}
-            </span>
-          </div>
-        );
+        return <TodoListItem key={todo.id} todo={todo} onDelete={onDelete} />;
       })}
     </div>
   );
-};
+}
 
-export default Todo;
+function TodoListItem(props) {
+  const { todo, onDelete } = props;
+
+  const onDeleteHandler = () => {
+    onDelete(todo.id);
+  };
+
+  return (
+    <div key={todo.id} style={{ backgroundColor: "#e2e8f0" }}>
+      <button
+        onClick={onDeleteHandler}
+        style={{ color: "#e53e3e", margin: "20px" }}
+      >
+        DEL
+      </button>
+      {todo.task}
+    </div>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const newInputHandler = todo => {
+    const newTodo = {
+      id: Math.random() * 100000,
+      task: todo
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const onDeleteHandler = id => {
+    let filteredTodos = todos.filter(todo => {
+      return todo.id !== id;
+    });
+    setTodos(filteredTodos);
+  };
+
+  return (
+    <div style={{ width: "50%" }}>
+      <Input onNewInput={newInputHandler} />
+      <TodoList todos={todos} onDelete={onDeleteHandler} />
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default App;
